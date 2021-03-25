f8f4ebb137199e8a86bb8ea5c2c3565d1c589b26
배진우 <bjwkor@naver.com>
2021-03-25 23:34:45 +0900

    Dynamic Route

---
+++ pages/books/[id].tsx
@@ -0,0 +1,9 @@
+import { Container } from "@material-ui/core";
+import React from "react";
+
+const BookDetail = (props) => {
+  console.log(props);
+  return <Container>Dynamic Route</Container>;
+};
+
+export default BookDetail;
--- pages/books/index.tsx
+++ pages/books/index.tsx
@@ -1,10 +1,20 @@
 import { Container, Typography } from "@material-ui/core";
 import React from "react";
+import Link from "next/link";
 
 const BooksPage = () => {
   return (
     <Container>
       <Typography>Books Page</Typography>
+      <Link href={"/books/[id]"} as={`/books/1`}>
+        Go to 1
+      </Link>
+      <Link href={"/books/[id]"} as={`/books/2`}>
+        Go to 2
+      </Link>
+      <Link href={"/books/[id]"} as={`/books/3`}>
+        Go to 3
+      </Link>
     </Container>
   );
 };
